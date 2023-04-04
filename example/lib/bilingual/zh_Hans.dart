2e2679d509aa59145336b73e59ceb4bd2b62b5c0
coker <codeLockers@outlook.com>
2023-04-04 14:22:00 +0800

    feat: update annotation package to v0.0.6

--- example/lib/bilingual/zh_Hans.dart
+++ example/lib/bilingual/zh_Hans.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 import 'en_us.dart';
 
 // ignore_for_file: camel_case_types
-@GoogleBilingual(['./lib'], 'zh_Hans', GOOGLE_SHEET_CREDENTIAL)
+@GoogleBilingual(['./lib'], 'zh_Hans', GOOGLE_SHEET_CREDENTIAL, true)
 const Locale zh = Locale('zh', 'Hans');
